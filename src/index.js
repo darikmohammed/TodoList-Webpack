78a262f3b5542896e5c6e3cfd84f244a1b41d2e3
darikmohammed <r.m.darik@gmail.com>
2022-06-01 03:21:54 +0300

    Add Javascript

--- src/index.js
+++ src/index.js
@@ -2,23 +2,47 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 
+const ulList = document.getElementById('dynamic-list');
 let toDoLists = [
   {
     description: 'Wash the dishes',
     completed: false,
-    index: 1,
+    index: 2,
   },
   {
     description: 'Complete to do list project',
     completed: false,
-    index: 2,
+    index: 1,
   },
 ];
 
 const uploadHtml = () => {
-  toDoLists.forEach((list)=>{
-    
-  })
+  toDoLists.sort((a, b) => {
+    return a.index - b.index;
+  });
+  toDoLists.forEach((list) => {
+    if (list.completed) {
+      ulList.innerHTML += ` 
+                        <li>
+                          <div class="list">
+                            <input type="checkbox" name="${list.index}" id="${list.index}" class="checkbox" checked>
+                            <label for="${list.index}"><s>${list.description}</s></label>
+                          </div>
+                          <i class="fa-solid fa-ellipsis-vertical"></i>
+                        </li>  
+                          `;
+    } else {
+      ulList.innerHTML += ` 
+                        <li>
+                          <div class="list">
+                            <input type="checkbox" name="${list.index}" id="${list.index}" class="checkbox">
+                            <label for="${list.index}">${list.description}</label>
+                          </div>
+                          <i class="fa-solid fa-ellipsis-vertical"></i>
+                        </li>  
+                          `;
+    }
+  });
 };
 
-uploadHtml(); 
+uploadHtml();
