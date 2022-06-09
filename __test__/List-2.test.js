26b8516eb2448fc3f49a75d66a6aaf7df7197860
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-06-09 13:22:00 -0600

    Create test for the edit description function

---
+++ __test__/List-2.test.js
@@ -0,0 +1,23 @@
+import List from '../src/modules/List.js';
+
+const list = new List();
+
+describe('This function', () => {
+  let todoList = [
+    {
+      description: 'Test File',
+      completed: false,
+      index: 1,
+    },
+  ];
+
+  test('checks if the description is updated', () => {
+    list.addList(todoList);
+
+    list.editTask(0, 'New value test');
+
+    todoList = list.getList();
+
+    expect(todoList[0].description).toBe('New value test');
+  });
+});
