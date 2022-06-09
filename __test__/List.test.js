5318e3877e486ba4802b07b54b482e88ffc971c5
darikmohammed <r.m.darik@gmail.com>
2022-06-09 19:18:42 +0300

    Add another test for adding on localstroage

--- __test__/List.test.js
+++ __test__/List.test.js
@@ -3,34 +3,29 @@ import List from '../src/modules/List.js';
 
 const list = new List();
 // global.localStorage = localStorageMock();
-describe('This function ', () => {
-  test('adds a list item correctly', () => {
-    const todoList = [
-      {
-        description: 'Test File',
-        completed: false,
-        index: 1,
-      },
-    ];
 
+describe('This function ', () => {
+  const todoList = [
+    {
+      description: 'Test File',
+      completed: false,
+      index: 1,
+    },
+  ];
+  test('adds a list item correctly and check the length', () => {
     list.addList(todoList);
     expect(list.getList().length).not.toBe(0);
   });
 
-  test('deletes a list item correctly', () => {
-    const todoList = [
-      {
-        description: 'Test File',
-        completed: false,
-        index: 1,
-      },
-    ];
+  test('adds a list item correctly and return it', () => {
+    expect(list.getList()).toStrictEqual(todoList);
+  });
 
+  test('deletes a list item correctly', () => {
     list.addList(todoList);
 
     list.deleteList(0);
 
     expect(list.getList().length).toBe(0);
   });
-
 });
