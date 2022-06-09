a5b136896ecd3ce2eeb4a89832c4dfd124c453a4
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-06-09 14:10:37 -0600

    Create function to test the checkbox and delete it from html

--- __test__/List-2.test.js
+++ __test__/List-2.test.js
@@ -25,7 +25,7 @@ describe('This function', () => {
     expect(todoList[0].completed).toBe(true);
   });
 
-  test(' and deletes the completed items', () => {
+  test('deletes the completed items', () => {
     todoList = list.getList();
     todoList[0].completed = true;
 
@@ -70,4 +70,63 @@ describe('This function', () => {
     const listLi = document.querySelector('.list label');
     expect(listLi.textContent).toBe('New value test');
   });
+
+  test('deletes edit the element from the dom', () => {
+
+
+    todoList = list.getList();
+    todoList[0].completed = true;
+
+    list.addList(todoList);
+
+    list.deleteCompleted();
+
+
+    const divList = document.getElementById('dynamic-list');
+    divList.innerHTML = ` 
+  <li id="${todoList[0].index - 1}" class = "todo-list" >
+    <div class="list">
+      <input type="checkbox" name="${todoList[0].index}" id="${
+      todoList[0].index
+    }" class="checkbox">
+      <label for="${todoList[0].index}">${todoList[0].description}</label>
+    </div>
+    <button type="button" id= "${todoList[0].index - 1}" class="deleteList">
+    <i class="fa-solid fa-trash-can"></i>
+    </button>
+  </li>  
+    `;
+
+    divList.firstElementChild.remove();
+
+    const listLi = document.querySelectorAll('#dynamic-list li');
+    expect(listLi).toHaveLength(0);
+
+  });
+
+  test('', () => {
+    list.addList(todoList);
+    todoList = list.getList();
+    todoList[0].completed = true;
+    list.addList(todoList);
+
+    const divList = document.getElementById('dynamic-list');
+
+  divList.innerHTML = ` 
+  <li id="${list.index - 1}" class = "todo-list" >
+    <div class="list">
+      <input type="checkbox" name="${list.index}" id="${
+  list.index
+}" class="checkbox ${todoList[0].completed}">
+      <label for="${list.index}"><s>${list.description}</s></label>
+    </div>
+    <button type="button" id= "${list.index - 1}" class="deleteList">
+    <i class="fa-solid fa-trash-can"></i>
+    </button>
+  </li>  
+    `;
+
+    const listLi = document.querySelector('.checkbox');
+    expect(listLi.className).toBe('checkbox true');
+  })
 });
