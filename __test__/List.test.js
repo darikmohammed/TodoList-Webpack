8ad775e32f9349dee9b92074138ba111c7d6ea94
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-06-09 09:26:55 -0600

    Create initial files for the testing phase

---
+++ __test__/List.test.js
@@ -0,0 +1,14 @@
+import List from '../src/modules/List.js'
+
+const list = new List();
+
+
+describe('This function ', () => {
+  test('adds a list item correctly', () => {
+    
+  });
+
+  test('deletes a list item correctly', () => {
+  
+  });
+});
