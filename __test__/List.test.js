48a7b55f076db4968499c161cf2b046bf11cf716
darikmohammed <r.m.darik@gmail.com>
2022-06-09 18:59:47 +0300

    Create an add test
    - mock local storage at jest
    - create a test to add to our local storage

--- __test__/List.test.js
+++ __test__/List.test.js
@@ -1,14 +1,21 @@
-import List from '../src/modules/List.js'
+import List from '../src/modules/List.js';
+// import localStorageMock from './__mock__/localStorage.js';
 
 const list = new List();
-
-
+// global.localStorage = localStorageMock();
 describe('This function ', () => {
   test('adds a list item correctly', () => {
-    
+    const todoList = [
+      {
+        description: 'Test File',
+        completed: false,
+        index: 1,
+      },
+    ];
+
+    list.addList(todoList);
+    expect(list.getList().length).not.toBe(0);
   });
 
-  test('deletes a list item correctly', () => {
-  
-  });
+  test('deletes a list item correctly', () => {});
 });
