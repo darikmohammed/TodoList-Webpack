71489cdc5047680c0384f6169a5ccbcf6be74e13
darikmohammed <r.m.darik@gmail.com>
2022-06-09 22:28:44 +0300

    Add test for updating the completed value

--- __test__/List-2.test.js
+++ __test__/List-2.test.js
@@ -10,14 +10,18 @@ describe('This function', () => {
       index: 1,
     },
   ];
-
+  list.addList(todoList);
   test('checks if the description is updated', () => {
-    list.addList(todoList);
-
     list.editTask(0, 'New value test');
 
     todoList = list.getList();
 
     expect(todoList[0].description).toBe('New value test');
+  });
+
+  test('check if the stauts completed is updated', () => {
+    todoList = list.getList();
+    todoList[0].completed = true;
+    expect(todoList[0].completed).toBe(true);
   });
 });
